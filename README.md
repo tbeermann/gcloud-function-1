8fbf83efd4ed325d540c8803b56fe83b12ce74f9
tim.beermann <tim.beermann@issinc.com>
2018-06-17 17:08:08 -0600

    intitial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # gcloud-function-1
 Google Cloud Serverless Function
+
+Simple function to access data from ADS-B Exchange
+
+https://public-api.adsbexchange.com/
---
+++ index.js
@@ -0,0 +1,20 @@
+
+const rp = require('request-promise');
+
+exports.adsDataExample = (req, res) => {
+    let uri = `https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?
+    lat=33.433638&
+    lng=-112.008113&
+    fDstL=0&
+    fDstU=100`
+
+    rp.get({
+        uri: uri,
+        json: true
+    }).then((response) => {
+        //Attach Response data to the Configuration Object
+        data = response;
+        res.setHeader('Content-Type', 'application/json');
+        res.send(data);
+    })
+}
